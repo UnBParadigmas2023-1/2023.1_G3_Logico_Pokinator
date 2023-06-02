5d55a672ff4fc58599098c2fbc21b8821857f43b
Daniel Porto <danielportods@gmail.com>
2023-06-02 04:42:08 -0300

    Revisa e ajusta a lógica da atualização da base.
    Co-authored-by: João Gabriel de Matos <joaogabriel.c.matos@gmail.com>
    Co-Authored-by: yuriAlves5 <yurivalerio12@gmail.com>

--- app/bc_atualizacoes.pl
+++ app/bc_atualizacoes.pl
@@ -1,48 +1,53 @@
 :- module(bc_atualizacoes, [atualizar_pokemons/3]).
 :- use_module([define_pokemons]).
 
-
 :-style_check(-discontiguous).
 :-style_check(-singleton).
 
 % --- Atualizar Pokemons --- %
-atualizar_pokemons(s, campo, valor):- 
-    get_by_paramm(campo,valor,PokemonsListados),
+atualizar_pokemons(s, 3, n) :-
+    get_by_paramm(3, n, PokemonsListados),
+    exclui_pokemons_da_lista(PokemonsListados).
+
+atualizar_pokemons(n, 3, n) :-
+    get_by_paramm(3, n, PokemonsListados),
     exclui_pokemons_fora_da_lista(PokemonsListados).
 
-atualizar_pokemons(n, campo, valor):- 
-        get_by_paramm(campo,valor,PokemonsListados),
-        exclui_pokemons_da_lista(PokemonsListados).
+atualizar_pokemons(s, 1, evo0) :-
+    get_by_paramm(1, evo0, PokemonsListados),
+    exclui_pokemons_da_lista(PokemonsListados).
 
+atualizar_pokemons(n, 1, evo0) :-
+    get_by_paramm(1, evo0, PokemonsListados),
+    exclui_pokemons_fora_da_lista(PokemonsListados).
 
+atualizar_pokemons(s, Campo, Valor) :- 
+    get_by_paramm(Campo, Valor, PokemonsListados),
+    exclui_pokemons_fora_da_lista(PokemonsListados).
 
+atualizar_pokemons(n, Campo, Valor) :- 
+    get_by_paramm(Campo, Valor, PokemonsListados),
+    exclui_pokemons_da_lista(PokemonsListados).
 
 exclui_pokemons_fora_da_lista(ListaDePokemons) :-
     get_all(TodosPokemons),
-    remove_out(TodosPokemons, ListaDePokemons).
-    
-
+    filter(TodosPokemons, ListaDePokemons, ListaFiltrada),
+    remove(ListaFiltrada).
 
 exclui_pokemons_da_lista(ListaDePokemons) :-
-    get_all(TodosPokemons),
-    remove_in(TodosPokemons, ListaDePokemons).
+    remove(ListaDePokemons).
 
-
-remove_out([H|T], Lista) :- 
-        member(H, Lista),
-        remove_out(T, Lista).
-
-remove_out([], Lista) :- !.
-
-remove_out([H|T], Lista) :- 
-            retract(pokemon(H, _, _, _, _, _, _)),
-            remove_out(T, Lista).
-    
-
-remove_in(All, [H|T]) :- (
-        member(H, All) -> 
-            retract(pokemon(H, _, _, _, _, _, _)) , remove_in(All, T) ;
-            remove_in(All, T)
+filter([], _, []).
+filter([H|T], Lista, ListaFiltrada) :- 
+    filter(T, Lista, AuxListaFiltrada),
+    ( member(H, Lista) ->
+        ListaFiltrada = AuxListaFiltrada
+    ;
+        ListaFiltrada = [H | AuxListaFiltrada]
     ).
-remove_in(All, []) :- !.
-    
+
+remove([]).
+remove([Head | Tail]) :-
+    retract(pokemon(Head, _, _, _, _, _)),
+    remove(Tail).
+
